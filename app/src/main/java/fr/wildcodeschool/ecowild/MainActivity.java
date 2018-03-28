f394a136b6161274509615e707fed58a5dc9d90b
Crock Nys <francoisnys@gmail.com>
2018-03-28 15:28:35 +0200

    premier jet

--- app/src/main/java/fr/wildcodeschool/ecowild/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/ecowild/MainActivity.java
@@ -1,17 +1,25 @@
 package fr.wildcodeschool.ecowild;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.GoogleMap;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMS_CALL_ID = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPosition();
 
         final Button buttonToLogIn = findViewById(R.id.button_to_log_in);
         buttonToLogIn.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +38,29 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(intentBecomeMember);
             }
         });
+
+        final Button shortcut = findViewById(R.id.cheat);
+        shortcut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentShortcut = new Intent(MainActivity.this, MapsActivity.class);
+                MainActivity.this.startActivity(intentShortcut);
+            }
+        });
+    }
+
+    private void checkPosition() {
+    if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
+
+    {
+
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        }, PERMS_CALL_ID);
+
+        return;
+    }
     }
 }
--- app/src/main/java/fr/wildcodeschool/ecowild/MapsActivity.java
+++ app/src/main/java/fr/wildcodeschool/ecowild/MapsActivity.java
@@ -1,46 +1,62 @@
 package fr.wildcodeschool.ecowild;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final int PERMS_CALL_ID = 1234;
+    private LocationManager lm;
 
+    //Pour recup la carte utiliser ses 2 attributs
+    //1er name ds layout=instance
+    //2eme: objet a linterieur pour pouvoir modif les données
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @SuppressWarnings("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
+        LatLng depart = new LatLng(43.6043896, 1.4433718000000226);
+
+        googleMap.setMyLocationEnabled(true);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(depart, 10));
+
+        // filtre afin de n'afficher que certaines données, en lien avec le json dans raw (créé).
+        MapStyleOptions carte = MapStyleOptions.loadRawResourceStyle(MapsActivity.this, R.raw.map_style);
+        googleMap.setMapStyle(carte);
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+
     }
 }
+
