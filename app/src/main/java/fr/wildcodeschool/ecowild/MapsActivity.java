5e2ad025636d008fb0e7f234539a5cfb4e04b8b4
Crock Nys <francoisnys@gmail.com>
2018-04-09 11:51:10 +0200

    sprint3 propre

--- app/src/main/java/fr/wildcodeschool/ecowild/MapsActivity.java
+++ app/src/main/java/fr/wildcodeschool/ecowild/MapsActivity.java
@@ -4,9 +4,14 @@ package fr.wildcodeschool.ecowild;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.location.LocationManager;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -37,26 +42,67 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //1er name ds layout=instance
     //2eme: objet a linterieur pour pouvoir modif les données
     private GoogleMap mMap;
-    int id;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        //Snack
+        Snackbar snackbar= Snackbar.make(this.findViewById(R.id.map), "N'oublier pas de vous connecter pour plus de fonctionnalités", Snackbar.LENGTH_INDEFINITE).setDuration(9000).setAction("Connexion", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MapsActivity.this,ConnectionActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        View snackbarView = snackbar.getView();
+        TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setMaxLines(3);
+        snackbar.show();
+
+
+        //Toast
+        /**LayoutInflater inflater = getLayoutInflater();
+         View layout = inflater.inflate(R.layout.toast,
+         (ViewGroup) findViewById(R.id.custom_toast_container));
+
+         TextView textToast = (TextView) layout.findViewById(R.id.text);
+         textToast.setText(R.string.Toast);
+
+         Toast toast = new Toast(getApplicationContext());
+         toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+         toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
+         toast.setDuration(Toast.LENGTH_LONG);
+         toast.setView(layout);
+         toast.show();
+         **/
+        //Map
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        Switch goList = findViewById(R.id.golist);
-        goList.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        // slide droite et gauche
+        drawerLayout = findViewById(R.id.drawerLayout);
+        Button buttonLeft = findViewById(R.id.button_left);
+
+        buttonLeft.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                Intent goToList = new Intent(MapsActivity.this, ListLocationActivity.class);
-                startActivity(goToList);
+            public void onClick(View v) {
+                drawerLayout.openDrawer(Gravity.LEFT);
             }
         });
 
+        Button buttonRigth = findViewById(R.id.buttonright);
+        buttonRigth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(Gravity.RIGHT);
+            }
+        });
 
     }
 
