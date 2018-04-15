93e6e3124c9f27a8f6a77ed8caaee66a077cb656
Clairehere <b_claire@hotmail.com>
2018-04-15 17:39:25 +0200

    design liste en fct type

--- app/src/main/java/fr/wildcodeschool/ecowild/ListAdapter.java
+++ app/src/main/java/fr/wildcodeschool/ecowild/ListAdapter.java
@@ -1,10 +1,12 @@
 package fr.wildcodeschool.ecowild;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -25,13 +27,28 @@ public class ListAdapter extends ArrayAdapter<ElementModel> {
         }
 
         TextView address = convertView.findViewById(R.id.address);
-        //TextView type = convertView.findViewById(R.id.type);
+        TextView type = convertView.findViewById(R.id.type);
        // TextView id = convertView.findViewById(R.id.smallid);
         LinearLayout list = convertView.findViewById(R.id.item_list);
 
         address.setText(gpsLocations.getAddress());
-       // type.setText(gpsLocations.getType());
+        type.setText(gpsLocations.getType());
        // id.setText(gpsLocations.getId());
+        LinearLayout liste =convertView.findViewById(R.id.item_list);
+
+
+        ImageView test = convertView.findViewById(R.id.iv_type);
+        if (gpsLocations.getType().equals("Verre")){
+            test.setBackgroundResource(R.drawable.verre);
+            //liste.setBackgroundColor(Color.alpha((long) 0.7).);
+            liste.setBackgroundColor(Color.parseColor("#bce7ca"));
+        }
+        else {
+            test.setBackgroundResource(R.drawable.papier);
+
+            liste.setBackgroundColor(Color.parseColor("#c7e0f6"));
+        }
+
 
         return convertView;
     }
