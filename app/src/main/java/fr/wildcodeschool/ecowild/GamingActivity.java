f9684598e457a201c6c874941d4a4036277855df
Clairehere <b_claire@hotmail.com>
2018-04-30 23:59:29 +0200

    interaction poubelle/objet

--- app/src/main/java/fr/wildcodeschool/ecowild/GamingActivity.java
+++ app/src/main/java/fr/wildcodeschool/ecowild/GamingActivity.java
@@ -12,14 +12,18 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GamingActivity extends AppCompatActivity {
+    public static int mENDGAME =0;
+    public static int mXp=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gaming);
 
+        //TODO: mettre en place gif de la fleche
         ImageView ivVerre = findViewById(R.id.iv_glass_table);
         ImageView ivVase = findViewById(R.id.iv_vase);
         ImageView ivFalseGlass = findViewById(R.id.iv_false_glass);
@@ -109,8 +113,174 @@ public class GamingActivity extends AppCompatActivity {
     View.OnDragListener dragListener = new View.OnDragListener() {
         @Override
         public boolean onDrag(View v, DragEvent event) {
-            //TODO: interaction avec les differentes poubelles
-            return false;
+            ImageView ivGlassTable = findViewById(R.id.iv_glass_table);
+            ImageView ivVase = findViewById(R.id.iv_vase);
+            ImageView ivFalseGlass = findViewById(R.id.iv_false_glass);
+            ImageView ivTacos = findViewById(R.id.iv_tacos);
+
+            ImageView ivPaper = findViewById(R.id.iv_paper);
+            ImageView ivCardboard = findViewById(R.id.iv_cardboard);
+
+            ImageView ivJar = findViewById(R.id.iv_jar);
+            ImageView ivGlass = findViewById(R.id.iv_glass);
+            TextView tvInfosGame = findViewById(R.id.tv_infos);
+            TextView tvScore =findViewById(R.id.tv_score);
+
+            final ConstraintLayout llBac = findViewById(R.id.linear_layout_bac);
+            final ConstraintLayout cWaste = findViewById(R.id.constraintlayout_waste);
+            int dragEvent = event.getAction();
+            final View view = (View) event.getLocalState();
+            switch (dragEvent) {
+                case DragEvent.ACTION_DRAG_ENTERED:
+
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+
+                    break;
+                case DragEvent.ACTION_DROP:
+
+                    //Dechets classiques
+                    if ((view.getId() == R.id.iv_glass_table || view.getId() == R.id.iv_vase || view.getId() == R.id.iv_false_glass || view.getId() == R.id.iv_tacos) && v.getId() == R.id.linear_layout_normal) {
+                        Toast.makeText(GamingActivity.this, R.string.positive_xp, Toast.LENGTH_SHORT).show();
+                        tvInfosGame.setText(" ");
+                        if (view.getId() == R.id.iv_glass_table) {
+                            ivGlassTable.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.positive1);
+                            mENDGAME += 1;
+                            mXp +=1;
+                        } else if (view.getId() == R.id.iv_vase) {
+                            ivVase.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.positive2);
+                            mENDGAME += 1;
+                            mXp +=1;
+                        } else if (view.getId() == R.id.iv_false_glass) {
+                            ivFalseGlass.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.positive3);
+                            mENDGAME += 1;
+                            mXp +=1;
+                        } else {
+                            ivTacos.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.positve4);
+                            mENDGAME += 1;
+                            mXp +=1;
+                        }
+                        tvScore.setText(getString(R.string.score)+Integer.valueOf(mXp).toString());
+
+                    } else if ((view.getId() == R.id.iv_glass_table || view.getId() == R.id.iv_vase || view.getId() == R.id.iv_tacos || view.getId() == R.id.iv_false_glass) && (v.getId() == R.id.linear_layout_paper || v.getId() == R.id.linear_layout_glass)) {
+                        Toast.makeText(GamingActivity.this, R.string.negative_xp, Toast.LENGTH_SHORT).show();
+                        if (view.getId() == R.id.iv_glass_table) {
+                            ivGlassTable.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.negative1);
+                            mENDGAME += 1;
+                            mXp -=1;
+                        } else if (view.getId() == R.id.iv_vase) {
+                            ivVase.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.negatuve2);
+                            mENDGAME += 1;
+                            mXp -=1;
+                        } else if (view.getId() == R.id.iv_false_glass) {
+                            ivFalseGlass.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.negative3);
+                            mENDGAME += 1;
+                            mXp -=1;
+                        }
+                        else {
+                            ivTacos.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.negative4);
+                            mENDGAME += 1;
+                            mXp -=1;
+
+                        }
+
+                        tvScore.setText(getString(R.string.score)+Integer.valueOf(mXp).toString());
+                    }
+
+                    //dechets verre
+                    if ((view.getId() == R.id.iv_glass || view.getId() == R.id.iv_jar) && v.getId() == R.id.linear_layout_glass) {
+                        Toast.makeText(GamingActivity.this, R.string.positive_xp, Toast.LENGTH_SHORT).show();
+                        if (view.getId() == R.id.iv_glass) {
+                            ivGlass.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.positive3);
+                            mENDGAME += 1;
+                            mXp +=1;
+                        } else {
+                            ivJar.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.positive2);
+                            mENDGAME += 1;
+                            mXp +=1;
+                        }
+
+                        tvScore.setText(getString(R.string.score)+Integer.valueOf(mXp).toString());
+
+                    } else if ((view.getId() == R.id.iv_glass || view.getId() == R.id.iv_jar) && (v.getId() == R.id.linear_layout_paper || v.getId() == R.id.linear_layout_normal)) {
+                        Toast.makeText(GamingActivity.this, R.string.negative_xp, Toast.LENGTH_SHORT).show();
+                        if (view.getId() == R.id.iv_glass) {
+                            ivGlass.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.negative5);
+                            mENDGAME += 1;
+                            mXp -=1;
+                        } else if (view.getId() == R.id.iv_jar) {
+                            ivJar.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.negative6);
+                            mENDGAME += 1;
+                            mXp -=1;
+                        }
+                        tvScore.setText(getString(R.string.score)+Integer.valueOf(mXp).toString());
+
+                    }
+
+                    //dechet papier
+                    if ((view.getId() == R.id.iv_cardboard || view.getId() == R.id.iv_paper) && v.getId() == R.id.linear_layout_paper) {
+                        Toast.makeText(GamingActivity.this, R.string.positive_xp, Toast.LENGTH_SHORT).show();
+                        if (view.getId() == R.id.iv_cardboard) {
+                            ivCardboard.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.positive1);
+                            mENDGAME += 1;
+                            mXp +=1;
+                        } else {
+                            ivPaper.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.positive3);
+                            mENDGAME += 1;
+                            mXp +=1;
+                        }
+
+                        tvScore.setText(getString(R.string.score)+Integer.valueOf(mXp).toString());
+
+                    } else if ((view.getId() == R.id.iv_cardboard || view.getId() == R.id.iv_paper) && (v.getId() == R.id.linear_layout_glass || v.getId() == R.id.linear_layout_normal)) {
+                        Toast.makeText(GamingActivity.this, R.string.negative_xp, Toast.LENGTH_SHORT).show();
+                        if (view.getId() == R.id.iv_cardboard) {
+                            ivCardboard.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.negative7);
+                            mENDGAME += 1;
+                            mXp -=1;
+                        } else if (view.getId() == R.id.iv_paper) {
+                            ivPaper.setVisibility(View.INVISIBLE);
+                            tvInfosGame.setText(R.string.negative8);
+                            mENDGAME += 1;
+                            mXp -=1;
+                        }
+                        tvScore.setText(getString(R.string.score)+Integer.valueOf(mXp).toString());
+
+                    }
+                    if (mENDGAME ==8) {
+                        if (mXp > 0) {
+                            Toast.makeText(GamingActivity.this, "Tu as obtenu " + Integer.valueOf(mXp).toString() + " XP en plus", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(GamingActivity.this, "Tu viens de perdre " + Integer.valueOf(mXp).toString() + " XP", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    llBac.setVisibility(View.INVISIBLE);
+                        cWaste.setVisibility(View.INVISIBLE);
+                    }
+
+                    break;
+
+
+            }
+
+            return true;
         }
     };
 }
