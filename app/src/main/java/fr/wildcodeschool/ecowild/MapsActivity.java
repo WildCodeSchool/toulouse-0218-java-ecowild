6bfcb931af1b6281a73f779ba98c0cd871416ff2
Crock Nys <francoisnys@gmail.com>
2018-05-04 11:42:48 +0200

    la distanc enous rapproche

--- app/src/main/java/fr/wildcodeschool/ecowild/MapsActivity.java
+++ app/src/main/java/fr/wildcodeschool/ecowild/MapsActivity.java
@@ -1023,7 +1023,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void updateMarker(Location location) {
 
         float distance;
-        float minDistance=1000;
+        float minDistance= 100;
 
 
         mUserPosition = new LatLng(location.getLatitude(), location.getLongitude());
@@ -1047,7 +1047,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
 
-         if ( Math.round(minDistance) < 2 ) {
+         if ( Math.round(minDistance) < 10 ) {
 
             AlertDialog.Builder popup = new AlertDialog.Builder(MapsActivity.this);
             popup.setTitle(R.string.alerte);
